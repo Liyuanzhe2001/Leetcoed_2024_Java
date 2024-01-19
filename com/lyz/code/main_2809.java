8c628fe4b34977911643dd3861cc7175427ee3af
Liyuanzhe2001 <619356147@qq.com>
2024-01-19 23:51:18 +0800

    2024-1-19

---
+++ com/lyz/code/main_2809.java
@@ -0,0 +1,43 @@
+package com.lyz.code;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author lkunk
+ * @date 2024年01月19日 23:50
+ * @description
+ */
+
+// TODO 2809
+public class main_2809 {
+    public int minimumTime(List<Integer> nums1, List<Integer> nums2, int x) {
+        int n = nums1.size(), s1 = 0, s2 = 0;
+        int[][] pairs = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            int a = nums1.get(i);
+            int b = nums2.get(i);
+            pairs[i][0] = a;
+            pairs[i][1] = b;
+            s1 += a;
+            s2 += b;
+        }
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+
+        int[] f = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            int a = pairs[i][0];
+            int b = pairs[i][1];
+            for (int j = i + 1; j > 0; j--) {
+                f[j] = Math.max(f[j], f[j - 1] + a + b * j);
+            }
+        }
+
+        for (int t = 0; t <= n; t++) {
+            if (s1 + s2 * t - f[t] <= x) {
+                return t;
+            }
+        }
+        return -1;
+    }
+}
