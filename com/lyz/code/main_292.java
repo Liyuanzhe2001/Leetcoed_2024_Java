779db89983928244f275c17beced7f6aa6334020
Liyuanzhe2001 <619356147@qq.com>
2024-02-04 22:14:27 +0800

    2024-2-4

---
+++ com/lyz/code/main_292.java
@@ -0,0 +1,13 @@
+package com.lyz.code;
+
+/**
+ * @author lkunk
+ * @date 2024年02月04日 22:13
+ * @description
+ */
+
+public class main_292 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
