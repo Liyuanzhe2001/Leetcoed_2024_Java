ae7caca9beebf113903c0608b2b4b0ea6aa9ef3a
Liyuanzhe2001 <619356147@qq.com>
2024-02-09 15:05:35 +0800

    2024-2-9

---
+++ com/lyz/code/main_236.java
@@ -0,0 +1,26 @@
+package com.lyz.code;
+
+import com.lyz.code.structure.TreeNode;
+
+/**
+ * @author lkunk
+ * @date 2024年02月09日 15:01
+ * @description
+ */
+
+public class main_236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+}
