dea37625da235384a68f06653e23e6ba5360eb8c
Liyuanzhe2001 <619356147@qq.com>
2024-03-17 21:01:01 +0800

    update

---
+++ com/lyz/code/main_64.java
@@ -0,0 +1,32 @@
+package com.lyz.code;
+
+import java.util.Arrays;
+
+/**
+ * @author lkunk
+ * @date 2024年03月17日 20:50
+ * @description
+ */
+
+public class main_64 {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        System.out.println(Arrays.deepToString(dp));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+}
