2b97ce0c74d67e71dee9a737987be38f44991113
Liyuanzhe2001 <619356147@qq.com>
2024-03-23 21:56:44 +0800

    2024-03-23

---
+++ com/lyz/code/main_2549.java
@@ -0,0 +1,13 @@
+package com.lyz.code;
+
+/**
+ * @author lkunk
+ * @date 2024年03月23日 21:56
+ * @description
+ */
+
+public class main_2549 {
+    public int distinctIntegers(int n) {
+        return n == 1 ? 1 : n - 1;
+    }
+}
