1e02883a2320b806fea58e4fb14f9382d0f27dd5
Liyuanzhe2001 <619356147@qq.com>
2024-04-06 21:38:51 +0800

    2024-04-06

---
+++ com/lyz/code/main_1483.java
@@ -0,0 +1,45 @@
+package com.lyz.code;
+
+/**
+ * @author lkunk
+ * @date 2024年04月06日 21:37
+ * @description
+ */
+
+public class main_1483 {
+    class TreeAncestor {
+        private int[][] pa;
+
+        public TreeAncestor(int n, int[] parent) {
+            int m = 32 - Integer.numberOfLeadingZeros(n); // n 的二进制长度
+            pa = new int[n][m];
+            for (int i = 0; i < n; i++)
+                pa[i][0] = parent[i];
+            for (int i = 0; i < m - 1; i++) {
+                for (int x = 0; x < n; x++) {
+                    int p = pa[x][i];
+                    pa[x][i + 1] = p < 0 ? -1 : pa[p][i];
+                }
+            }
+        }
+
+        public int getKthAncestor(int node, int k) {
+            int m = 32 - Integer.numberOfLeadingZeros(k); // k 的二进制长度
+            for (int i = 0; i < m; i++) {
+                if (((k >> i) & 1) > 0) { // k 的二进制从低到高第 i 位是 1
+                    node = pa[node][i];
+                    if (node < 0) break;
+                }
+            }
+            return node;
+        }
+
+        // 另一种写法，不断去掉 k 的最低位的 1
+        public int getKthAncestor2(int node, int k) {
+            for (; k > 0 && node != -1; k &= k - 1) {
+                node = pa[node][Integer.numberOfTrailingZeros(k)];
+            }
+            return node;
+        }
+    }
+}
